77a8853a8c5b840ccaf42159da75dd053c7c4405
Jaylon Taylor <jaylon.taylor2000@gmail.com>
2024-02-29 02:43:55 -0600

    Updated

--- check_palindrome.py
+++ check_palindrome.py
@@ -1,9 +1,9 @@
 def check_palindrome(text):
-    if text == text[::-1]:
-        print(f"{text} is a palindrome")
-    else:
-        print(f"{text} is not a palindrome")
+  if text == text[::-1]:
+    print(f"{text} is a palindrome")
+  else:
+    print(f"{text} is not a palindrome")
 
 
-text = "did"
+text = input("Enter text to check for palindrome: ")
 check_palindrome(text)
